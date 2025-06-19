b078dd0d0f6baae321a661755a28c3be512bf1e6
MISAKI\toyam <test-mail@test.com>
2025-06-19 22:11:06 +0900

    posts追加

--- components/navbar.vue
+++ components/navbar.vue
@@ -2,6 +2,7 @@
   <nav>
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLink to="/posts">Post</NuxtLink>
     <NuxtLink :to="{ name: 'users-list' }">User List</NuxtLink>
   </nav>
 </template>
