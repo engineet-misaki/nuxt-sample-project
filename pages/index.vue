6fb5d08e81d65efd9ab790ae8f4c1677b29efa90
MISAKI\toyam <test-mail@test.com>
2025-06-23 22:00:47 +0900

    そろえた

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,7 @@
 <template>
   <div>
       <h1>Main Page</h1>
-      
-  <h1 class="text-3xl font-bold underline">
+      <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
   </div>
